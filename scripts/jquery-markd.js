cbd02a4c18716ea774db81e611e3389bb6821ba1
Tom-Marius Olsen <tom-marius.olsen@dagbladet.no>
2012-08-21 09:26:56 +0200

    Additional fixes for 'contenteditable'. Most importantly, fixed selection-range.

--- scripts/jquery-markd.js
+++ scripts/jquery-markd.js
@@ -3,10 +3,10 @@
 	jquery-markd
 	--------
 
-	@file       jquery-markd.js
-	@version    
-	@date       18.08.12
-	@author     Tom-Marius Olsen <tmol@dagbladet.no>
+	@file	   jquery-markd.js
+	@version	
+	@date	   18.08.12
+	@author	 Tom-Marius Olsen <tmol@dagbladet.no>
 
 	Copyright (c) 2012 DB Medialab AS <http://www.dbmedialab.no/>
 
@@ -44,8 +44,7 @@
 				if(el.is('textarea')){
 					return el.val();
 				} else {
-      				// Fixme: Why does not el.innerText work? Is should right?
-      				return document.getElementById(el.attr('id')).innerText;
+					return document.getElementById(el.attr('id')).innerText;
 				};
 			};
 			
@@ -78,7 +77,7 @@
 				$('body').prepend(p.el);
 				//Create aliases for the head and body inside the iframe for easy access
 				p.head = $('head', p.el[0].contentWindow.document);
-				p.body = $('body', p.el[0].contentWindow.document);				
+				p.body = $('body', p.el[0].contentWindow.document);
 				//Add the preview-theme to the iframe
 				p.head.append('<link rel="stylesheet" href="'+opts.theme+'" />');
 				return p;
@@ -89,8 +88,8 @@
 			 * @param	{object} object created using createPreview()
 			 * @return	{object} empty object
 			 **/
-			function deletePreview(p){				
-				if(p.el != undefined) p.el.remove();				
+			function deletePreview(p){
+				if(p.el != undefined) p.el.remove();
 				return p = {};
 			}
 			
@@ -109,44 +108,62 @@
 				};
 			};
 			
+			/**
+			 * Return the selection for a element.
+			 * @param	{element}
+			 * @return	{object} obj.start and obj.end
+			 **/
+			function _getSelection(el){
+				var selection = el.selection();
+				if(!el.is('textarea')){
+					//If we are not using a textarea newlines does not count in the selection-range,
+					//so we need to add these manually.
+					var t = _getContent(el);
+					var newlines = t.substr(0, selection.start).match(/\n/g).length;
+					selection.start += newlines;
+					selection.end += newlines;
+				};
+				return selection;
+			};
+			
 			// !Bind keyboard commands when the textfield recives focus.
 			_element.bind('focus', function(event){
 				
 				// !Bind tab to fake-enable tabbing
 				Mousetrap.bind('tab', function(){
-					var selection = _element.selection();
+					var s = _getSelection(_element);
 					var t = _getContent(_element);
-					t = insert(t, selection.end, '    ');
+					t = insert(t, s.end, '	');
 					_setContent(_element, t);
 					return false;
 				});
 				
 				// !Bind ctrl-k to links
 				Mousetrap.bind('ctrl+k', function(){
-					var selection = _element.selection();
+					var s = _getSelection(_element);
 					var t = _getContent(_element);
-					t = insert(t, selection.end, ']()');
-					t = insert(t, selection.start, '[');
+					t = insert(t, s.end, ']()');
+					t = insert(t, s.start, '[');
 					_setContent(_element, t);
 					return false;
-				});				
+				});
 				
 				// !Bind ctrl-b to bold
 				Mousetrap.bind('ctrl+b', function(){
-					var selection = _element.selection();
+					var s = _getSelection(_element);
 					var t = _getContent(_element);
-					t = insert(t, selection.end, '__');
-					t = insert(t, selection.start, '__');
+					t = insert(t, s.end, '__');
+					t = insert(t, s.start, '__');
 					_setContent(_element, t);
 					return false;
 				});
 				
 				// !Bind ctrl-i to emphasis/italic
 				Mousetrap.bind('ctrl+i', function(){
-					var selection = _element.selection();
+					var s = _getSelection(_element);
 					var t = _getContent(_element);
-					t = insert(t, selection.end, '_');
-					t = insert(t, selection.start, '_');
+					t = insert(t, s.end, '_');
+					t = insert(t, s.start, '_');
 					_setContent(_element, t);
 					return false;
 				});
@@ -171,47 +188,61 @@
 								height: _element.outerHeight()
 							});
 							//Render the text into the iframe
-							//preview.body.html( _parser( _element.val() ) );
 							preview.body.html( _parser(_getContent(_element)) );
 							
 						} else {
 							//Remove and delete the preview
 							preview = deletePreview(preview);
 						};
-					};					
+					};
 					//Return false to prevent default browser behavior.
 					return false;
 				});
 				
 				// !Bind ctrl-f as a toggle for fullscreen-mode.
 				Mousetrap.bind('ctrl+f', function(){
-				    //Enter fullscreen
-				    if(!_is_fullscreen){
-				    	//Flag fullscreen as true
-				    	_is_fullscreen = true;
-				    	//Give editor fullscreen styles
-				    	_element.addClass('fullscreen');
-				    	//Create a new iframe to preview the text
-				    	preview = createPreview();
-				    	//Give the preview fullscreen styles
-				    	preview.el.addClass('fullscreen');
-				    	//Update the preview on each keyup
-				    	_element.bind('keyup', function(event){
-				    		preview.body.html( _parser( _element.val() ) );
-				    	});
-				    //Exit fullscreen
-				    } else {
-				    	//Flag fullscreen as false
-				    	_is_fullscreen = false;
-				    	//Remove fullscreen styles from editor
-				    	_element.removeClass('fullscreen');
-				    	//Unbind preview-update on each keyup 
-				    	_element.unbind('keyup');
-				    	//Remove and delete the preview
-				    	preview = deletePreview(preview);
-				    };
-				    //Return false to prevent default browser behavior.
-				    return false;
+					//Enter fullscreen
+					if(!_is_fullscreen){
+						//Flag fullscreen as true
+						_is_fullscreen = true;
+						//Give editor fullscreen styles
+						_element.addClass('fullscreen');
+						//Create a new iframe to preview the text
+						preview = createPreview();
+						//Give the preview fullscreen styles
+						preview.el.addClass('fullscreen');
+						//Update the preview on each keyup
+						_element.bind('keyup', function(event){
+							preview.body.html( _parser( _getContent(_element) ) );
+						});
+						// Bind esc to close fullscreen view
+						Mousetrap.bind('esc', function(){
+							//Flag fullscreen as false
+							_is_fullscreen = false;
+							//Remove fullscreen styles from editor
+							_element.removeClass('fullscreen');
+							//Unbind preview-update on each keyup 
+							_element.unbind('keyup');
+							//Unbind esc
+							Mousetrap.unbind('esc');
+							//Remove and delete the preview
+							preview = deletePreview(preview);
+						});
+					//Exit fullscreen
+					} else {
+						//Flag fullscreen as false
+						_is_fullscreen = false;
+						//Remove fullscreen styles from editor
+						_element.removeClass('fullscreen');
+						//Unbind preview-update on each keyup 
+						_element.unbind('keyup');
+						//Unbind esc
+						Mousetrap.unbind('esc');
+						//Remove and delete the preview
+						preview = deletePreview(preview);
+					};
+					//Return false to prevent default browser behavior.
+					return false;
 				});
 
 			});
